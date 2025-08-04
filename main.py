0cc8b8853b2a6b8d24afbe45eff8588c50c9d323
Luis Marrero <lmarrero1070@gmail.com>
2025-08-04 19:03:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4321", "https://luismarrero.me"],
+    allow_origins=["http://localhost:4321", "https://luismarrero.me", "https://www.luismarrero.me"],
     allow_methods=["POST"],
     allow_headers=["*"])
 
