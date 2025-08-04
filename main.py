ce523fcab499a014175c47ceda7a2b37a7b5f20d
Luis Marrero <lmarrero1070@gmail.com>
2025-08-04 17:59:59 -0400

    refactor: reorganize middleware setup for clarity

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from parser import parse_markdown
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4321", "https://luismarrero.me"],
+    allow_methods=["POST"],
+    allow_headers=["*"])
 
 class MarkdownInput(BaseModel):
     text: str
